97d9efa97c66d6c43b045386ba4da5587f93bb18
C22HK <c22hk77kurd20@gmail.com>
2026-09-13 08:55:07 -0600

    Phase 2 Batch 2: load editorial layer on shared public surfaces

--- js/nav-auth.js
+++ js/nav-auth.js
@@ -1,3 +1,4 @@
+import './content-editorial.js';
 import { auth, db, getDoc } from './firebase-init.js';
 import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js';
 import { doc } from 'https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js';
